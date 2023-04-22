b5f76b0341045d82b011fce37f1c38ccb95f0307
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-04-22 04:46:33 +0200

    Release

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# vinted-view-bot
- Simple vinted views bot. Works with shit proxies.
+# Vinted View Boost Bot
+
+A simple, multi-threaded Python script to increase the view count of a Vinted item using rotating proxies and randomized user agents.
+
+## Features
+
+- Multi-threading to speed up the process
+- Rotating proxies to bypass request limitations
+- Randomized user agents to mimic real users
+
+## Requirements
+
+- Python 3.6+
+- `requests` module
+- `fake-useragent` module
+
+## Installation
+
+1. Clone the repository or download the source code:
+
+\`\`\`bash
+git clone https://github.com/kWAYTV/vinted-view-bot.git
+\`\`\`
+
+2. Install the required packages:
+
+\`\`\`bash
+pip install -r requirements.txt
+\`\`\`
+
+3. Add your proxies to the `proxies.txt` file, one proxy per line. Use `user:pass@ip:port` format.
+
+## Usage
+
+1. Run the script:
+
+\`\`\`bash
+python main.py
+\`\`\`
+
+2. Enter the Vinted link and the number of threads when prompted.
+
+3. The script will start sending requests to the provided Vinted link, and the view count will increase.
+
+## Note
+
+This script is for educational purposes only. Misusing the script may violate Vinted's terms of service. Use it responsibly and at your own risk.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ main.py
@@ -0,0 +1,92 @@
+import threading, requests, os
+from itertools import cycle
+from colorama import Fore, Style
+from pystyle import Colors, Colorate, Center
+from fake_useragent import UserAgent
+
+logo = """
+██╗   ██╗██╗███╗   ██╗████████╗███████╗██████╗ 
+██║   ██║██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗
+██║   ██║██║██╔██╗ ██║   ██║   █████╗  ██║  ██║
+╚██╗ ██╔╝██║██║╚██╗██║   ██║   ██╔══╝  ██║  ██║
+ ╚████╔╝ ██║██║ ╚████║   ██║   ███████╗██████╔╝
+  ╚═══╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═════╝"""
+
+# Clear the console.
+clear = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear")
+
+def check_files():
+    if not os.path.exists("proxies.txt"):
+        with open("proxies.txt", "w") as f:
+            f.write("Put your proxies here. (user:pass@ip:port)")
+        print(f"{Style.DIM}ERROR - {Style.RESET_ALL}You need to input proxies in proxies.txt")
+        input(f"{Style.DIM}EXIT - {Style.RESET_ALL}Press enter to exit...")
+        exit()
+
+    if os.stat("proxies.txt").st_size == 0:
+        print(f"{Style.DIM}ERROR - {Style.RESET_ALL}You need to input proxies in proxies.txt")
+        input(f"{Style.DIM}EXIT - {Style.RESET_ALL}Press enter to exit...")
+        exit()
+
+class VintedViewer:
+    def __init__(self, link, threads):
+        self.link = link
+        self.lock = threading.Lock()
+        self.threads = threads
+        self.ctr = 0
+        self.session = requests.Session()
+        self.proxy_pool = self.load_proxies()
+        self.proxy = next(self.proxy_pool)
+        self.set_proxy()
+        self.user_agent = UserAgent()
+
+    def load_proxies(self):
+        with open("proxies.txt", "r") as f:
+            proxies = f.read().splitlines()
+        return cycle(proxies)
+
+    def set_proxy(self):
+        self.proxy = next(self.proxy_pool)
+        self.session.proxies = {'http': f"http://{self.proxy}", 'https': f'http://{self.proxy}'}
+
+    def safe_print(self, arg):
+        with self.lock:
+            print(arg)
+
+    def thread_starter(self):
+        headers = {
+            "User-Agent": self.user_agent.random
+        }
+        try:
+            r = self.session.get(self.link, headers=headers)
+            if r.status_code == 200:
+                with self.lock:
+                    self.ctr += 1
+                self.safe_print(f"{Style.DIM}OK - {Style.RESET_ALL}Views +1 (Total: {self.ctr})")
+            else:
+                self.safe_print(f"{Style.DIM}ERROR - {Style.RESET_ALL}Error: {r.status_code} for proxy {self.proxy}")
+        except requests.exceptions.RequestException as e:
+            self.safe_print(f"{Style.DIM}ERROR - {Style.RESET_ALL}Error: {e} for proxy {self.proxy}")
+
+    def run(self):
+        while True:
+            if threading.active_count() <= self.threads:
+                try:
+                    threading.Thread(target=self.thread_starter).start()
+                except:
+                    pass
+            self.set_proxy()
+
+def main():
+    clear()
+    print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, logo, 1)))
+    print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, "────────────────────────────────────────────\n", 1)))
+    print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, "Starting...", 1)))
+    check_files()
+    link = input(f"{Style.DIM}Input - {Style.RESET_ALL}Vinted link: ")
+    threads = int(input(f"{Style.DIM}Input - {Style.RESET_ALL}Threads: "))
+    viewer = VintedViewer(link, threads)
+    viewer.run()
+
+if __name__ == "__main__":
+    main()
